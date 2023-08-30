a7f49e91784155c1b4ff0f854055ffe3ecf6f7fc
Jhony Alpala <jhoniher95@gmail.com>
2023-08-30 17:13:27 -0500

    Ejercicio finalizado

--- EjercicioPropuesto14.java
+++ EjercicioPropuesto14.java
@@ -10,8 +10,8 @@ public class EjercicioPropuesto14 {
         double numero, cuadrado, cubo;
         System.out.println("Ingrese un numero: ");
         numero=entrada.nextDouble();
-        cuadrado=Math.pow(numero,2);
-        cubo=Math.pow(numero, 3);
+        cuadrado=Math.pow(numero,2); //Se calcula el cuadrado
+        cubo=Math.pow(numero, 3); //Se calcula el cubo
         System.out.println("El cuadrado de "+numero+ " es: "+cuadrado);
         System.out.println("El cubo de "+numero+" es: "+cubo);
     }
